008e130f8d39875b178d8788aa6b80a8883b9269
renosaza <222202868+renosaza@users.noreply.github.com>
2026-07-16 15:10:06 +1000

    test: materialize URLProtocol request body streams

--- GT2SpotifyTests/MockURLProtocol.swift
+++ GT2SpotifyTests/MockURLProtocol.swift
@@ -11,8 +11,15 @@ final class MockURLProtocol: URLProtocol, @unchecked Sendable {
             client?.urlProtocol(self, didFailWithError: URLError(.badServerResponse))
             return
         }
+
         do {
-            let (response, data) = try handler(request)
+            var materializedRequest = request
+            if materializedRequest.httpBody == nil,
+               let stream = materializedRequest.httpBodyStream {
+                materializedRequest.httpBody = try Self.readAll(from: stream)
+            }
+
+            let (response, data) = try handler(materializedRequest)
             client?.urlProtocol(self, didReceive: response, cacheStoragePolicy: .notAllowed)
             client?.urlProtocol(self, didLoad: data)
             client?.urlProtocolDidFinishLoading(self)
@@ -27,5 +34,26 @@ final class MockURLProtocol: URLProtocol, @unchecked Sendable {
         let configuration = URLSessionConfiguration.ephemeral
         configuration.protocolClasses = [MockURLProtocol.self]
         return URLSession(configuration: configuration)
+    }
+
+    private static func readAll(from stream: InputStream) throws -> Data {
+        stream.open()
+        defer { stream.close() }
+
+        let bufferSize = 1_024
+        let buffer = UnsafeMutablePointer<UInt8>.allocate(capacity: bufferSize)
+        defer { buffer.deallocate() }
+
+        var data = Data()
+        while true {
+            let bytesRead = stream.read(buffer, maxLength: bufferSize)
+            if bytesRead < 0 {
+                throw stream.streamError ?? URLError(.cannotDecodeContentData)
+            }
+            if bytesRead == 0 {
+                return data
+            }
+            data.append(buffer, count: bytesRead)
+        }
     }
 }
